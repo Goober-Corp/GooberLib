fa9f9d59db11b3dc6e79fd03f9f208ed68c69e1f
kr1viah <kravpolet@gmail.com>
2026-05-28 12:26:13 +0200

    its so much better now

--- src/main/java/com/goobercorp/gooberlib/api/widgets/NumberWidgetProviders.java
+++ src/main/java/com/goobercorp/gooberlib/api/widgets/NumberWidgetProviders.java
@@ -3,6 +3,7 @@ package com.goobercorp.gooberlib.api.widgets;
 import com.goobercorp.gooberlib.gui.EvilSliderWidget;
 import com.goobercorp.gooberlib.gui.EvilStringWidget;
 import com.goobercorp.gooberlib.interfaces.WidgetProvider;
+import com.goobercorp.gooberlib.option.individual.primitive.CharOption;
 import com.goobercorp.gooberlib.option.individual.primitive.NumberOption;
 import net.minecraft.text.Text;
 
@@ -18,6 +19,6 @@ public class NumberWidgetProviders {
 	}
 
 	public static <T extends NumberOption<T>> WidgetProvider<T> field() {
-		return ((theOption, x, y, width, height) -> new EvilStringWidget(theOption.name(), x, y, width, height, theOption::setFromString, theOption.getPredicate(), theOption.getNumberValue().toString()));
+		return ((theOption, x, y, width, height) -> new EvilStringWidget(theOption.name(), x, y, width, height, theOption::setFromString, theOption.getPredicate(), theOption instanceof CharOption c ? String.valueOf(c.value) : theOption.getNumberValue().toString()));
 	}
 }
